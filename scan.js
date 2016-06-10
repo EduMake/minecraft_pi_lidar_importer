294af41b83bbb6a73826640ad8e125fbe8184cc4
Tech Group <meggleton@utcsheffield.org.uk>
2016-06-10 13:15:50 +0000

    Changes to https://github.com/EduMake/minecraft-pi-vec3 For 3D point based maths Also implements queue system for asyncronous API requests

--- scan.js
+++ scan.js
@@ -1,7 +1,10 @@
+#! /usr/bin/env node
+
 var fs = require('fs');
 var LIDAR = require("./lidardata.js");
-var Minecraft = require('minecraft-pi');
-var Blocks = require('minecraft-pi/lib/blocks.json');
+var Minecraft = require('minecraft-pi-vec3');
+var Blocks = require('minecraft-pi-vec3/lib/blocks.json');
+var v = require("minecraft-pi-vec3/lib/vec3_directions.js");
 
 var DangerousBlocks= [
 	"SAPLING", "WATER_FLOWING",	"LAVA_FLOWING", "LEAVES", "BED", "COBWEB", "GRASS_TALL", "WOOL",	"FLOWER_YELLOW",
@@ -31,7 +34,7 @@ var cli = commandLineArgs([
 	{ name: 'gridref', type: String, alias:'g', defaultOption: true, defaultValue:'SK 35526 86610', description:"Grid Reference to centre on" },
 	{ name: 'save', alias: 'n', type: String, defaultValue:"", description:"Save Grid Ref with this name" },
 	{ name: 'load', alias: 'l', type: Number, description:sKnowns },
-	{ name: 'build', alias: 'b', type: Boolean, defaultValue: false, description:"Build this area in Minecraft"},
+	{ name: 'build', alias: 'b', type: Boolean, defaultValue: true, description:"Build this area in Minecraft"},
 	{ name: 'terrain_block', alias: 't', type: Number, defaultValue: Blocks.GRASS, description: sBlocks},
 	{ name: 'surface_block', alias: 's', type: Number, defaultValue: Blocks.IRON_BLOCK},
 	{ name: 'quarter', alias: 'q', type: Number, defaultValue: 0},
@@ -84,63 +87,63 @@ patch.debug = options.debug;
 
 
 const MinY = -63;
-const MaxY = 63;
+const MaxY = 64;
 
 var qx = options.quarter % 2;
 var qz = Math.floor(options.quarter / 2);
-var cx = 0-(128/2)+(qx * 128);
-var cz = 0-(128/2)+(qz * 128);
+var c = v(0-(128/2)+(qx * 128) , MinY , 0-(128/2)+(qz * 128));
 
 if (options.centre) {
-	var cx = 0;
-	var cz = 0;
+	c = v(0, MinY, 0);
 }
 
 function doStuff() {
 	console.log("LIDAR Loaded");
-	
 	var oZone = patch.getZone(iSize);
-	//console.log(oZone);
 	
 	if(options.build == false){
-		console.log(Out);
+		
 	} else {
 		var client = new Minecraft('localhost', 4711, function() {
-			client.chat('HELLO! IMPORTING LIDAR DATA INTO MINECRAFT PI EDITION. PLEASE ENJOY!.');
-
+			client.chat('IMPORTING LIDAR DATA INTO MINECRAFT PI EDITION.');
 			var half = Math.round(iSize/2);
-
-			client.setPos(cx, Math.round(oZone.iMaxHeight - oZone.iMinHeight) + MinY +10, cz);
-
+			var OverWatch = c.up(Math.round((oZone.iMaxHeight - oZone.iMinHeight)/options.resolution) + 5);
+			client.setPos(OverWatch);
+			
+			var SouthWest = c.offset(-1 * half, 0, -1 * half);
+			var NorthEast = c.offset(half, 0, half);
+			var NorthEastTop = c.offset(half, MaxY, half);
+			
 			client.chat('Clearing!.');
-			client.setBlocks(cx-half,  MinY,     cz-half,  half,  MinY,   cx+half, client.blocks['COBBLESTONE']);
-			client.setBlocks(cx-half,  MinY +1 , cz-half,  half,  MaxY,   cz+half, client.blocks['AIR']);
+			// Floor across the area
+			client.setBlocks(SouthWest, NorthEast, client.blocks['COBBLESTONE']);
+			// Air from just above the floor to the top of the world
+			client.setBlocks(SouthWest.up(1), NorthEastTop, client.blocks['AIR']);
 			client.chat('Area Cleared!.');
 
 			var WaterMCHeight = MinY - 1;
 			if(options.water > -1) {
-				WaterMCHeight = Math.round(options.water / options.resolution) + MinY;
-				console.log("iMinHeight", oZone.iMinHeight, "options.water", options.water, "WaterMCHeight", WaterMCHeight );
-				client.setBlocks(cx-half,  MinY , cz-half,  half, WaterMCHeight ,  cz+half, client.blocks['WATER_STATIONARY']);    
+				WaterMCHeight = Math.round(options.water / options.resolution);
+				//Replace the floor and above with water
+				client.setBlocks(SouthWest, NorthEast.up(WaterMCHeight), client.blocks['WATER_STATIONARY']);    
 			}
 		
 			for(var i = 0 ; i < oZone.DTM.length; i++) { //north direction
 				for(var j = 0 ; j < oZone.DTM[0].length; j++) { //east direction
-					var x = cx + j - half;
-					var z = cz + i - half;
-					var TerrainMCHeight = Math.round((oZone.DTM[i][j] - oZone.iMinHeight)  / options.resolution) + MinY  ;
-					var SurfaceMCHeight = TerrainMCHeight + Math.round( oZone.Heights[i][j] / options.resolution);
+					var FloorPoint = SouthWest.offset(j, 0, i);
+					var TerrainPoint = FloorPoint.up(Math.round((oZone.DTM[i][j] - oZone.iMinHeight)  / options.resolution));
+					var SurfacePoint = TerrainPoint.up(Math.round( oZone.Heights[i][j] / options.resolution));
 					
-					if(TerrainMCHeight > WaterMCHeight) {
-						client.setBlocks(x, MinY , z, x, TerrainMCHeight , z, options.terrain_block);
+					if(TerrainPoint.y > WaterMCHeight + MinY) {
+						client.setBlocks(FloorPoint, TerrainPoint, options.terrain_block);
 					}
 					
-					if(SurfaceMCHeight > TerrainMCHeight) {
-						client.setBlocks(x, TerrainMCHeight+1, z, x, SurfaceMCHeight, z, options.surface_block);
+					if(SurfacePoint.y > TerrainPoint.y) {
+						client.setBlocks(TerrainPoint.up(1), SurfacePoint, options.surface_block);
 					}
 				}
-				if(i % 10 == 0) {
-					var message = 'i = '+i+ " z= "+ z;
+				if(i % 10 == 0 || (i + 1) == iSize) {
+					var message = "Importing "+Math.ceil(100*i/iSize)+ " % Complete";
 					console.log(message);
 					client.chat(message);
 				}
@@ -148,7 +151,7 @@ function doStuff() {
 	  
 			console.log("Done");
 			client.chat('Done');
-			client.setPos(cx, Math.round(oZone.iMaxHeight - oZone.iMinHeight) + MinY +10, cz);
+			client.setPos(OverWatch);
 			client.end();
 		});
 	}
